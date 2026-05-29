17f81236155be623c59bb006c745ce769e6a6ddd
Ben Wolthuis <ben.wolthuis@gmail.com>
2026-05-29 17:54:21 +0200

    fix(llort) corrected damage and health stats

--- core/src/main/java/com/nebbii/zagdx/EnemyLlort.java
+++ core/src/main/java/com/nebbii/zagdx/EnemyLlort.java
@@ -33,8 +33,8 @@ public class EnemyLlort extends Enemy {
 
         setWidth(48);
         setHeight(64);
-        setHealth(600);
-        setDamage(60);
+        setHealth(60);
+        setDamage(50);
         setDefense(24);
         setSpeed(110f);
 
